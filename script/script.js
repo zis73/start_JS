fa5f905ed764306f90704b55b9a193c439442be5
Eldar Akavov <akavoveldar3005@gmail.com>
2020-08-07 22:03:32 +0300

    ci(script.js):lesson02

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
-let money;
-let income; 
-let addExpenses; 
-let deposit;
-let mission;
-let period;
-alert('Первая домашка на гитхабе!');
-console.log('Привет куратор');
+let money = 50000,
+income = 'Фриланс', 
+addExpenses = 'ЖКХ, питание, бензин ', 
+deposit = true,
+mission = 2000000,
+period = 10,
+budgetDay = money/30;
+
+console.log('type of money: ' + typeof(money));
+console.log('type of income: ' + typeof(income));
+console.log('type of deposit: ' + typeof(deposit));
+console.log('length of addExpenses: '+ addExpenses.length);
+console.log("Период равен " + period + " месяцям" + "\n"+ "Цель заработать " + mission + " рублей/долларов/гривен/юаней");
+console.log(addExpenses.toLowerCase().split(", "));
+console.log('budgetDay: ' + budgetDay);
