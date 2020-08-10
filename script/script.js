6d36f242bba869c32eaa5028532247fb8c723f9c
Eldar Akavov <akavoveldar3005@gmail.com>
2020-08-10 14:14:35 +0300

    fix(script.js):Проставил пробелы, изменил строку с typeOf

--- script/script.js
+++ script/script.js
@@ -4,12 +4,10 @@ addExpenses = 'ЖКХ, питание, бензин ',
 deposit = true,
 mission = 2000000,
 period = 10,
-budgetDay = money/30;
+budgetDay = money / 30;
 
-console.log('type of money: ' + typeof(money));
-console.log('type of income: ' + typeof(income));
-console.log('type of deposit: ' + typeof(deposit));
-console.log('length of addExpenses: '+ addExpenses.length);
-console.log("Период равен " + period + " месяцям" + "\n"+ "Цель заработать " + mission + " рублей");
+console.log('type of money: ' + typeof(money) + "\n" + 'type of income: ' + typeof(income) + "\n" +'type of deposit: ' + typeof(deposit));
+console.log('length of addExpenses: ' + addExpenses.length);
+console.log("Период равен " + period + " месяцям" + "\n" + "Цель заработать " + mission + " рублей");
 console.log(addExpenses.toLowerCase().split(", "));
 console.log('budgetDay: ' + budgetDay);
