27dddcbd3542c79f9846d6e18da736e53aa34f90
Eldar Akavov <akavoveldar3005@gmail.com>
2020-08-11 21:27:20 +0300

    fix(script.js): Проставил пробелы

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 const money = 50000,
-income = 'Фриланс', 
-addExpenses = 'ЖКХ, питание, бензин ', 
-deposit = true,
-mission = 2000000,
-period = 10,
-budgetDay = money / 30;
+  income = 'Фриланс', 
+  addExpenses = 'ЖКХ, питание, бензин ', 
+  deposit = true,
+  mission = 2000000,
+  period = 10,
+  budgetDay = money / 30;
 
 console.log('type of money: ' + typeof(money) + "\n" + 'type of income: ' + typeof(income) + "\n" +'type of deposit: ' + typeof(deposit));
 console.log('length of addExpenses: ' + addExpenses.length);
