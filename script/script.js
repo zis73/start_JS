5ee2469a4f10648b41b4fe9b440dd70ac8c5e141
Eldar Akavov <akavoveldar3005@gmail.com>
2020-08-12 07:33:12 +0300

    fix(script.js):Поправил скрипт

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@
 let money = 50000,
   deposit = true,
   mission = 2000000,
-  budgetDay = money / 30;
+  budgetDay = money / 30,
   addExpenses = 'ЖКХ, питание, бензин ';
 const  income = 'Фриланс',  
   period = 10;
@@ -16,9 +16,9 @@ console.log(addExpenses.toLowerCase().split(", "));
 console.log('budgetDay: ' + budgetDay);
 
   money = +prompt('Ваш месячный доход?');
-console.log(money + typeof money);
+console.log(money);
   addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(addExpenses + typeof addExpenses);
+console.log(addExpenses);
   deposit = confirm('Есть ли у вас депозит в банке?');
 console.log(deposit);
 const expenses1 = prompt('Введите обязательную статью расходов?');
@@ -31,7 +31,7 @@ const amount2 = +prompt('Во сколько это обойдется?');
 console.log(amount2);
 const budgetMonth = money - (amount1 + amount2);
 console.log("Бюджет на месяц: " + budgetMonth);
-  missionMonth = mission / budgetMonth;
+const  missionMonth = mission / budgetMonth;
 console.log('Цель будет достигнута за: ' + Math.ceil(missionMonth) + ' месяцев');
   budgetDay = Math.ceil(budgetMonth / 30);
 console.log('Бюджет на день: ' + budgetDay);
@@ -47,11 +47,11 @@ if(budgetDay >= 1200){
 }
 
 switch(budgetDay > 0){
-  case budgetDay >= 1200: console.log('asd');
+  case budgetDay >= 1200: console.log(''У вас высокий уровень дохода');
   break;
-  case 600 <= budgetDay < 1200: console.log('bvc');
+  case 600 <= budgetDay < 1200: console.log('У вас средний уровень дохода');
   break;
-  case 0 <= budgetDay < 600: console.log('qwe');
+  case 0 <= budgetDay < 600: console.log('К сожалению у вас уровень дохода ниже среднего');
   break;
-  default: console.log('!!!');
+  default: console.log('Что-то пошло не так');
 }
